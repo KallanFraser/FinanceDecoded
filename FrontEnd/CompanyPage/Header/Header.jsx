57f0ff1a6c82fe2ef4b6819b70906057dbc23b8f
KallanFraser <117051126+KallanFraser@users.noreply.github.com>
2025-01-04 09:19:57 -0800

    Git ignore added

--- FrontEnd/CompanyPage/Header/Header.jsx
+++ FrontEnd/CompanyPage/Header/Header.jsx
@@ -4,9 +4,19 @@ import React from "react";
 
 import "./Header.css";
 
+function formatValue(value) {
+	const cappedValue = Math.floor(value * 100) / 100;
+	const formattedValue = cappedValue >= 0 ? `+${cappedValue}` : cappedValue;
+	return formattedValue;
+}
+
+function getClassName(value) {
+	return value >= 0 ? "market-price-positive" : "market-price-negative";
+}
+
 export const CompanyHeader = ({ stockQuote }) => {
-	//Extract some stuff
 	console.log("Quote: ", stockQuote);
+
 	return (
 		<div id="company-header">
 			<div id="header-line-one">
@@ -17,8 +27,27 @@ export const CompanyHeader = ({ stockQuote }) => {
 				<button className="header-button">Compare</button>
 			</div>
 			<div id="header-line-two">
-				<h1>{stockQuote.bid}</h1>
-				<h2>{stockQuote.regularMarketChange}</h2>
+				<div id="regular-market-section">
+					<h1>{stockQuote.regularMarketPrice}</h1>
+					<h2 className={getClassName(stockQuote.regularMarketChange)}>
+						{formatValue(stockQuote.regularMarketChange)}
+					</h2>
+					<h2 className={getClassName(stockQuote.regularMarketChangePercent)}>
+						({formatValue(stockQuote.regularMarketChangePercent)})%
+					</h2>
+				</div>
+				{stockQuote.tradeable && (
+					<div id="after-market-section">
+						<h1>{stockQuote.postMarketPrice}</h1>
+						<h2 className={getClassName(stockQuote.postMarketChange)}>
+							{formatValue(stockQuote.postMarketChange)}
+						</h2>
+						<h2 className={getClassName(stockQuote.postMarketChangePercent)}>
+							({formatValue(stockQuote.postMarketChangePercent)})%
+						</h2>
+						<h3>(Post Trading Hours)</h3>
+					</div>
+				)}
 			</div>
 		</div>
 	);
